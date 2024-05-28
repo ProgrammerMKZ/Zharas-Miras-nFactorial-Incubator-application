d373a6c1cd74ed8c018bba309ddb9607e777d0cf
ProgrammerMKZ <zharas@nisa.edu.kz>
2024-05-28 20:29:25 +0500

    views for registration and login

--- Events_Miras/main/views.py
+++ Events_Miras/main/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 
 # Create your views here.
 
 def index(request):
     return render(request, 'main/index.html')
+
+def login_to_account(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return render(request, 'main/index.html')
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="main/login.html", context={"login_form":form})
+
+def register_to_account(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST, request.FILES)
+        if form.is_valid:
+            user = form.save()
+            login(request, user)
+            return render(request, 'main/login.html')
+        else:
+            return render(request, 'main/registration.html', {'register_form': form})
